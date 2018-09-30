8f8d1f042882cc2495f70fd9f2b55cd14319eb22
Nicole W <Nicole.Wilkins2018@gmail.com>
2018-09-29 21:03:12 -0400

    makes GIF start and stop

--- gif.js
+++ gif.js
@@ -7,7 +7,7 @@ var topics = ["French Bulldog", "Mastiff", "Puggle"];
 function displayDogInfo () {
 
     var dog = $(this).attr("data-name");
-    var queryURl = "https://api.giphy.com/v1/gifs/search?q=" + dog + "&api_key=1VzzyqPCQIAxhSBkMgMSQN5w6ARuwdLn";
+    var queryURl = "https://api.giphy.com/v1/gifs/search?q=" + dog + "&api_key=1VzzyqPCQIAxhSBkMgMSQN5w6ARuwdLn&limit=10";
 
     //creates AJAX call for buttons clicked
     $.ajax({
@@ -16,6 +16,7 @@ function displayDogInfo () {
     }).then(function (response) {
 
         var results = response.data;
+        $("#dog-view").empty();
         for (var i = 0; i < results.length; i++) {
 
             //takes the photo if rating of pg-13 or less
@@ -34,7 +35,10 @@ function displayDogInfo () {
                 var dogImage = $("<img>");
 
                 //gives image src from results
-                dogImage.attr("src", results[i].images.fixed_height.url);
+                dogImage.attr("src", results[i].images.fixed_height_still.url);
+                dogImage.attr("data-still", results[i].images.fixed_height_still.url);
+                dogImage.attr("data-animate", results[i].images.fixed_height.url);
+                dogImage.attr("data-state", "still")
 
                 //appents rating paragraph to GIF
                 GIFdiv.append(p);
@@ -50,7 +54,7 @@ function displayDogInfo () {
 //function for displaying dogs
 function renderButtons() {
     //removes old topics before new ones are displayed
-    $("#dog-view").empty();
+    $("#new-buttons").empty();
 
     for (var i =0; i < topics.length; i++) {
 
@@ -68,6 +72,7 @@ function renderButtons() {
 $("#add-dog").on("click", function (event){
     event.preventDefault();
     var dog = $("#dog-input").val().trim();
+    $("#dog-input").val("");
     //adds to topic array
     topics.push(dog);
     renderButtons();
@@ -78,3 +83,16 @@ $("#add-dog").on("click", function (event){
 $(document).on("click", ".dog-button", displayDogInfo);
 
 renderButtons();
+
+//new function to change data-state of img
+$(document).on("click", "img", function (){
+    if($(this).attr("data-state") === "still") {
+        console.log("stuff here")
+    $(this).attr("src", $(this).attr("data-animate"))
+    $(this).attr("data-state", "animate")
+    }
+    else { $(this).attr("src", $(this).attr("data-still"))
+    $(this).attr("data-state", "still")
+        console.log("then this")
+}
+})
